6a320007edf09980d7b6bac5a56a20032e1f3b15
mcarlsn <madelynsc18@gmail.com>
2025-10-23 10:50:16 -0400

    10/23

--- week_08_main_1.py
+++ week_08_main_1.py
@@ -1,6 +1,12 @@
 # Import your packages, modules, global constants first
 import self_py_fun.week_08_example_fun as ef
 import numpy as np
+# Notes from class on 10/17:
+# You NEED to impact packages etc. at the beginning, or else PyCharm will give you an error.
+# You can also import your OWN function(s).
+# You have two options for importing function or packages
+import self_py_fun.week_08_example_fun as ef2 # here, you'll need to specify .ef2 everytime you're calling the function. If you DON'T want to do that, try the next one
+from self_py_fun.week_08_example_fun import* # using this import statement, you WON'T have to specify .ef2 everytime you're calling the function
 
 
 # Call variables
@@ -10,3 +16,5 @@ print(ef.alpha)
 print(ef.message_hello('Tianwen'))
 print(ef.fn_cubic(3))
 print(np.array([1.0]))
+
+# Whenever you write a customized function
